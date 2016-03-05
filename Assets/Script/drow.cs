fa017c196ae38b72fe0c3f0285488d1956a55801
soutetu <yasumotosoutetsu@kouhonsoutetsu-no-MacBook-Air.local>
2016-03-05 17:04:24 +0900

    Unityでブラックジャック

---
+++ Assets/Script/drow.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class drow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+		transform.position = new Vector3 (0, 0, 0);
+	}
+}
