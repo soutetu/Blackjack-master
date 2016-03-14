eff09dea19f3891c1b6cf36fb038627621e0171e
soutetu <yasumotosoutetsu@kouhonsoutetsu-no-MacBook-Air.local>
2016-03-14 23:09:15 +0900

    BlackJack
    カード出現するようになった。

--- Assets/Script/drow.cs
+++ Assets/Script/drow.cs
@@ -5,7 +5,8 @@ public class drow : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-	
-		transform.position = new Vector3 (0, 0, 0);
+		GameObject card = (GameObject)Resources.Load ("Trumps/trump_0");
+		//card = (GameObject)Resources.Load("./Trumps/trump_0");
+		Instantiate(card, new Vector3(0,0,0), transform.rotation);
 	}
 }
